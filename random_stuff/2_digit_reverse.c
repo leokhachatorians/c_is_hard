2e2412fc0fc65d3c75e38bcdf5eb3374e390c1cd
leokhachatorians <leokhachatorians@gmail.com>
2016-08-04 21:27:42 -0700

    Fixed Tabs

--- random_stuff/2_digit_reverse.c
+++ random_stuff/2_digit_reverse.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-	int num, a, b;
+    int num, a, b;
 
-	printf("Enter a two digit number: ");
-	scanf("%2d", &num);
+    printf("Enter a two digit number: ");
+    scanf("%2d", &num);
 
-	a = num % 10;
-	b = num / 10;
+    a = num % 10;
+    b = num / 10;
 
-	printf("The reversal is: %d%d\n", a, b);
-	return 0;
+    printf("The reversal is: %d%d\n", a, b);
+    return 0;
 }
--- random_stuff/reverse_date.c
+++ random_stuff/reverse_date.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void){ 
-	int month, day, year;
+    int month, day, year;
 
-	printf("Enter date [mm/dd/yyyy]: ");
-	scanf("%d/%d/%d", &month, &day, &year);
-	printf("%d%d%d", year, month, day);
-	return 0;
+    printf("Enter date [mm/dd/yyyy]: ");
+    scanf("%d/%d/%d", &month, &day, &year);
+    printf("%d%d%d", year, month, day);
+    return 0;
 }
